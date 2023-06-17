ec248ca69d63e34231d672664aa2bfc9ed26750e
amirshafri <amir_shafri@yahoo.com>
2023-06-17 23:00:35 +0800

    pause button add

---
+++ mathFun/StopButton.swift
@@ -0,0 +1,20 @@
+//
+//  StopButton.swift
+//  mathFun
+//
+//  Created by Amir Shafri on 17/06/2023.
+//
+
+import SwiftUI
+
+struct StopButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StopButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StopButton()
+    }
+}
