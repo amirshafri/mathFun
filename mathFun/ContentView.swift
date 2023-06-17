17dfb0399713f81f3099ef5d42b1a63fe244cc51
amirshafri <amir.shafri@reldyn.co>
2023-06-17 17:35:36 +0800

    Initial commit

--- mathFun/ContentView.swift
+++ mathFun/ContentView.swift
@@ -8,19 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var correctAnswer = 0
+    @State private var choiceArray : [Int] = [0,1,2,3]
+    @State private var firstNumber = 0
+    @State private var secondNumber = 0
+    @State private var difficulty = 1000
+    @State private var score = 0
+    @State private var timerValue = 0
+    @State private var timer: Timer? = nil
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(firstNumber) + \(secondNumber)")
+                .font(.largeTitle)
+                .bold()
+            HStack{
+                ForEach(0..<2){
+                    index in Button {
+                        answerIsCorrect(answer: choiceArray[index])
+                        generateAnswer()
+                    } label: {
+                        AnswerButton(number: choiceArray[index]).frame(height: 100)
+                    }
+                }
+            }
+            HStack{
+                ForEach(2..<4){
+                    index in Button {
+                        answerIsCorrect(answer: choiceArray[index])
+                        generateAnswer()
+                    } label: {
+                        AnswerButton(number: choiceArray[index])
+                    }
+                }
+            }
+            Text("Time: \(timerValue) Score: \(score)")
+                .font(.headline)
+                .bold()
         }
-        .padding()
+        .onAppear(perform: startTimer)
+        .onDisappear(perform: stopTimer)
     }
+    
+    func generateAnswer(){
+        firstNumber = Int.random(in: 0...(difficulty/2))
+        secondNumber = Int.random(in: 0...(difficulty/2))
+        var answerList = [Int]()
+        correctAnswer = firstNumber + secondNumber
+        for _ in 0...2{
+            answerList.append(Int.random(in: 0...difficulty))
+        }
+        answerList.append(correctAnswer)
+        choiceArray = answerList.shuffled()
+        
+    }
+    
+    func answerIsCorrect(answer: Int){
+        self.score = answer == correctAnswer ? self.score + 1 : self.score - 1
+    }
+    
+    func startTimer() {
+        generateAnswer()
+          timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+              timerValue += 1
+          }
+      }
+      
+      func stopTimer() {
+          timer?.invalidate()
+          timer = nil
+      }
+    
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(.top)
+            .environment(\.sizeCategory, .large)
+            
+            
+            
+            
     }
 }
