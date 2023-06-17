13c80e592fec38936d9684ae7171fe86a64efaaa
amirshafri <amir.shafri@reldyn.co>
2023-06-17 14:52:30 +0800

    Initial Commit

---
+++ mathFun/mathFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathFunApp.swift
+//  mathFun
+//
+//  Created by Amir Shafri on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct mathFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
